70fc527738c3df2d0c4022218d57746ed2277823
Rubab Arshad <160515174+rubabarshad@users.noreply.github.com>
2024-10-16 22:57:00 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Responsive from "./responsive/page";
+import Responsive2 from "./responsive2/page";
+
+export default function Home() {
+  return (<div>
+    <Responsive/>
+    <Responsive2/>
+      </div>
+  )}
